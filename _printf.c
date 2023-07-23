00894ba5af526419bd98387712f5fe35b188e762
Yusufibra <yusufibra075@gmail.com>
2023-07-23 06:14:21 -0700

    more of it

--- _printf.c
+++ _printf.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
-  * _printf - Printf function
+ * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -49,6 +51,7 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
